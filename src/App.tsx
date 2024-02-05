c0981ae1ace4b3f9dd40a6b300e871b89feea1d5
qiuzhanghua <qiuzhanghua@icloud.com>
2024-02-05 08:58:53 +0800

    chore: pnpm up -Lri

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import type { Component } from 'solid-js';
+import {Button} from "@opentj/solid";
 
 const App: Component = () => {
   return (
+      <>
       <h1 class="text-3xl font-bold underline text-purple-600">
         Hello world!
       </h1>
+      <Button class={"border-solid"}></Button>
+      </>
   );
 };
 
